9c3585a4ba108d153ca34a71f2eefed2f7a0c246
Gregory Mankes <gregory.mankes@temporal.io>
2026-01-21 10:20:20 -0500

    add goreleaser, release wf, and cloud client user agent

--- temporalcloudcli/cloud.go
+++ temporalcloudcli/cloud.go
@@ -39,7 +39,9 @@ func (c *CloudCommand) GetAPIKey(ctx context.Context) (string, error) {
 }
 
 func newCloudClient(cctx *CommandContext) (*cloudclient.Client, error) {
-	opts := cloudclient.Options{}
+	opts := cloudclient.Options{
+		UserAgent: fmt.Sprintf("temporal-cloud/%s", VersionString()),
+	}
 	if cctx.RootCommand.Server != "" {
 		opts.HostPort = cctx.RootCommand.Server
 	}
