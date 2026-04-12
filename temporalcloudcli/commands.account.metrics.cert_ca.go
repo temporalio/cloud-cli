50eb8d8257050fd6f4a3ed61f585ee952ff3c36c
Abhinav Prasad Nekkanti <10552725+anekkanti@users.noreply.github.com>
2026-04-12 16:13:21 -0700

    Account metrics mgmt commands

---
+++ temporalcloudcli/commands.account.metrics.cert_ca.go
@@ -0,0 +1,176 @@
+package temporalcloudcli
+
+import (
+	"bytes"
+	"encoding/base64"
+	"errors"
+
+	accountv1 "go.temporal.io/cloud-sdk/api/account/v1"
+	cloudservice "go.temporal.io/cloud-sdk/api/cloudservice/v1"
+	"google.golang.org/protobuf/proto"
+
+	"github.com/temporalio/cloud-cli/internal/cert"
+	"github.com/temporalio/cloud-cli/temporalcloudcli/internal/printer"
+)
+
+func (c *CloudAccountMetricsCertCaListCommand) run(cctx *CommandContext, _ []string) error {
+	client, err := cctx.GetCloudClient(c.ClientOptions)
+	if err != nil {
+		return err
+	}
+
+	res, err := client.GetAccount(cctx, &cloudservice.GetAccountRequest{})
+	if err != nil {
+		return err
+	}
+
+	certs, err := cert.ParseCACerts(res.Account.GetSpec().GetMetrics().GetAcceptedClientCa())
+	if err != nil {
+		return err
+	}
+
+	return cctx.Printer.PrintStructured(certs, printer.StructuredOptions{})
+}
+
+func (c *CloudAccountMetricsCertCaCreateCommand) run(cctx *CommandContext, _ []string) error {
+	newCerts, err := readAndParseCACerts(c.CaCertificateOptions)
+	if err != nil {
+		return err
+	}
+
+	client, err := cctx.GetCloudClient(c.ClientOptions)
+	if err != nil {
+		return err
+	}
+
+	res, err := client.GetAccount(cctx, &cloudservice.GetAccountRequest{})
+	if err != nil {
+		return err
+	}
+
+	account := res.Account
+	existingData := account.GetSpec().GetMetrics().GetAcceptedClientCa()
+
+	var existingCerts []cert.CACert
+	if len(existingData) > 0 {
+		existingCerts, err = cert.ParseCACerts(existingData)
+		if err != nil {
+			return err
+		}
+	}
+
+	existingFingerprints := map[string]struct{}{}
+	for _, ec := range existingCerts {
+		existingFingerprints[ec.Fingerprint] = struct{}{}
+	}
+
+	var certsToAdd []cert.CACert
+	for _, nc := range newCerts {
+		if _, exists := existingFingerprints[nc.Fingerprint]; !exists {
+			certsToAdd = append(certsToAdd, nc)
+		}
+	}
+
+	bundleBytes, err := encodeCACertBundle(append(existingCerts, certsToAdd...))
+	if err != nil {
+		return err
+	}
+
+	newSpec := proto.Clone(account.Spec).(*accountv1.AccountSpec)
+	if newSpec.Metrics == nil {
+		newSpec.Metrics = &accountv1.MetricsSpec{}
+	}
+	newSpec.Metrics.AcceptedClientCa = bundleBytes
+
+	rv := account.ResourceVersion
+	if c.ResourceVersion != "" {
+		rv = c.ResourceVersion
+	}
+
+	resp, err := client.UpdateAccount(cctx, &cloudservice.UpdateAccountRequest{
+		Spec:             newSpec,
+		ResourceVersion:  rv,
+		AsyncOperationId: c.AsyncOperationId,
+	})
+	return cctx.GetPoller(client, c.AsyncOperationOptions).HandleUpdateOperation(cctx, resp, err)
+}
+
+func (c *CloudAccountMetricsCertCaDeleteCommand) run(cctx *CommandContext, _ []string) error {
+	certsToRemove, err := readAndParseCACerts(c.CaCertificateOptions)
+	if err != nil {
+		return err
+	}
+
+	yes, err := cctx.GetPrompter().PromptYes("Delete")
+	if err != nil {
+		return err
+	}
+	if !yes {
+		return errors.New("Aborting delete.")
+	}
+
+	client, err := cctx.GetCloudClient(c.ClientOptions)
+	if err != nil {
+		return err
+	}
+
+	res, err := client.GetAccount(cctx, &cloudservice.GetAccountRequest{})
+	if err != nil {
+		return err
+	}
+
+	account := res.Account
+	existingCerts, err := cert.ParseCACerts(account.GetSpec().GetMetrics().GetAcceptedClientCa())
+	if err != nil {
+		return err
+	}
+
+	fingerprintsToRemove := map[string]struct{}{}
+	for _, rc := range certsToRemove {
+		fingerprintsToRemove[rc.Fingerprint] = struct{}{}
+	}
+
+	var newBundle []cert.CACert
+	for _, existing := range existingCerts {
+		if _, ok := fingerprintsToRemove[existing.Fingerprint]; ok {
+			continue
+		}
+		newBundle = append(newBundle, existing)
+	}
+
+	bundleBytes, err := encodeCACertBundle(newBundle)
+	if err != nil {
+		return err
+	}
+
+	newSpec := proto.Clone(account.Spec).(*accountv1.AccountSpec)
+	if newSpec.Metrics == nil {
+		newSpec.Metrics = &accountv1.MetricsSpec{}
+	}
+	newSpec.Metrics.AcceptedClientCa = bundleBytes
+
+	rv := account.ResourceVersion
+	if c.ResourceVersion != "" {
+		rv = c.ResourceVersion
+	}
+
+	resp, err := client.UpdateAccount(cctx, &cloudservice.UpdateAccountRequest{
+		Spec:             newSpec,
+		ResourceVersion:  rv,
+		AsyncOperationId: c.AsyncOperationId,
+	})
+	return cctx.GetPoller(client, c.AsyncOperationOptions).HandleUpdateOperation(cctx, resp, err)
+}
+
+// encodeCACertBundle encodes a slice of CACerts as a concatenated PEM bundle.
+func encodeCACertBundle(certs []cert.CACert) ([]byte, error) {
+	var out [][]byte
+	for _, c := range certs {
+		data, err := base64.StdEncoding.DecodeString(c.Base64EncodedData)
+		if err != nil {
+			return nil, err
+		}
+		out = append(out, data)
+	}
+	return bytes.Join(out, []byte("\n")), nil
+}
