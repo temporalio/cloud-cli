c9cdd2e5ea17e3d86265403ca0e87cac073a6c1c
Jeri Lane <jeri.lane@temporal.io>
2026-07-01 09:56:20 -0700

    Make CommandHarness accessible in non-integration tests

--- temporalcloudcli/common_test.go
+++ temporalcloudcli/common_test.go
@@ -5,7 +5,11 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"regexp"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -16,6 +20,132 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
+
+type CommandHarness struct {
+	*require.Assertions
+	t       *testing.T
+	Options temporalcloudcli.CommandOptions
+	// Defaults to a context closed on close or test complete
+	Context context.Context
+	// Can be used to cancel context given to commands (simulating interrupt)
+	CancelContext context.CancelFunc
+	Stdin         bytes.Buffer
+}
+
+func NewCommandHarness(t *testing.T) *CommandHarness {
+	h := &CommandHarness{Assertions: require.New(t), t: t}
+	h.Context, h.CancelContext = context.WithCancel(context.Background())
+	t.Cleanup(h.Close)
+	return h
+}
+
+// Reentrant, called after test by default, cancels context
+func (h *CommandHarness) Close() {
+	// Cancel context
+	if h.CancelContext != nil {
+		h.CancelContext()
+	}
+}
+
+// Pieces must appear in order on the line and not overlap
+func (h *CommandHarness) ContainsOnSameLine(text string, pieces ...string) {
+	h.NoError(AssertContainsOnSameLine(text, pieces...))
+}
+
+func AssertContainsOnSameLine(text string, pieces ...string) error {
+	// Build regex pattern based on pieces
+	pattern := ""
+	for _, piece := range pieces {
+		if pattern != "" {
+			pattern += ".*"
+		}
+		pattern += regexp.QuoteMeta(piece)
+	}
+	regex, err := regexp.Compile(pattern)
+	if err != nil {
+		return err
+	}
+	// Split into lines, then check each piece is present
+	lines := strings.Split(text, "\n")
+	for _, line := range lines {
+		if regex.MatchString(line) {
+			return nil
+		}
+	}
+	return fmt.Errorf("pieces not found in order on any line together")
+}
+
+func TestAssertContainsOnSameLine(t *testing.T) {
+	require.Error(t, AssertContainsOnSameLine("a b c", "b", "a"))
+	require.Error(t, AssertContainsOnSameLine("a\nb c", "a", "b"))
+	require.NoError(t, AssertContainsOnSameLine("aba", "b", "a"))
+	require.NoError(t, AssertContainsOnSameLine("a b a", "b", "a"))
+	require.NoError(t, AssertContainsOnSameLine("axb", "a", "b"))
+	require.NoError(t, AssertContainsOnSameLine("a a", "a", "a"))
+}
+
+func (h *CommandHarness) Eventually(
+	condition func() bool,
+	waitFor time.Duration,
+	tick time.Duration,
+	msgAndArgs ...interface{},
+) {
+	h.t.Helper()
+	// We cannot use require.Eventually because it was poorly developed to run the
+	// condition function in a goroutine which means it can run after complete or
+	// have other race conditions. Don't even need a complicated ticker because it
+	// doesn't need to be interruptible.
+	for start := time.Now(); time.Since(start) < waitFor; {
+		if condition() {
+			return
+		}
+		time.Sleep(tick)
+	}
+	h.Fail("condition did not evaluate to true within timeout", msgAndArgs...)
+}
+
+func (h *CommandHarness) T() *testing.T {
+	return h.t
+}
+
+type CommandResult struct {
+	Err    error
+	Stdout bytes.Buffer
+	Stderr bytes.Buffer
+}
+
+func (h *CommandHarness) Execute(args ...string) *CommandResult {
+	// Copy options, update as needed
+	res := &CommandResult{}
+	options := h.Options
+	// Set stdio
+	options.Stdin = &h.Stdin
+	options.Stdout = &res.Stdout
+	options.Stderr = &res.Stderr
+	// Set args
+	options.Args = args
+	// Capture error
+	options.Fail = func(err error) {
+		if res.Err != nil {
+			panic("fail called twice, just failed with " + err.Error())
+		}
+		res.Err = err
+	}
+
+	// Run
+	ctx, cancel := context.WithCancel(h.Context)
+	h.t.Cleanup(cancel)
+	defer cancel()
+	h.t.Logf("Calling: %v", strings.Join(args, " "))
+	temporalcloudcli.Execute(ctx, options)
+	if res.Stdout.Len() > 0 {
+		h.t.Logf("Stdout:\n-----\n%s\n-----", &res.Stdout)
+	}
+	if res.Stderr.Len() > 0 {
+		h.t.Logf("Stderr:\n-----\n%s\n-----", &res.Stderr)
+	}
+	return res
+}
 
 func newTestCommandContext(t *testing.T, buf *bytes.Buffer) *temporalcloudcli.CommandContext {
 	t.Helper()
